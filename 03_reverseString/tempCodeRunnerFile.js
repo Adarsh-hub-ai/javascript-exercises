c9dce99775b49b6da18031009e8e45c48bc53486
Adarsh-singh-git <as8266749@gmail.com>
2025-01-08 12:12:50 +0530

    Updated existing files

---
+++ 03_reverseString/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+    for (let i = str.length - 1; i >= 0; i--) {
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+    let sum = 0
+    let areIntegers = args.every(num => Number.isInteger(num))
+    let hasNegative = args.some(num => num < 0)
+    if (!hasNegative && areIntegers) {
+        
+        for (let i = Math.min(...args); i <= Math.max(...args); i++) {
 
+            sum += i
+        }
+    } else {
+        return 'ERROR'
+    }
+
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
