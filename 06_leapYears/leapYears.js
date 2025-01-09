4ed8ae26f1a9c02396a3b298b1c242f2fed407ea
Adarsh-singh-git <as8266749@gmail.com>
2025-01-09 13:05:33 +0530

    practiced the problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+switch (true) {
+    case (year % 4 === 0 && year % 100 !== 0):
+        return true
+    case(year % 4 !== 0):
+    return false
+    case(year % 4 === 0 && year % 100 === 0):
+    return checkDivisibleBy400(year)
+    default:
+        break;
 
+        function checkDivisibleBy400(year) {
+            if (year % 400 === 0) {
+                return true
+            }else {
+                 return false
+            }
+        }
+}
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
