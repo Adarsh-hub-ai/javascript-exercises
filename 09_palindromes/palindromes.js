7729b7730a6464d60d7bd50849d0e0126ca8b957
Adarsh-singh-git <as8266749@gmail.com>
2025-01-13 12:36:18 +0530

    completed the task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let new_string = string.replace(/[^a-zA-z0-9]/g, "").toLowerCase()
+    let reversed_string = new_string.split("").reverse().join("").toLowerCase()
+    return (reversed_string === new_string)
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
