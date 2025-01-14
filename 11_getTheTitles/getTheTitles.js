cc8b8b98a396da809162be1bd97726afe59514df
Adarsh-singh-git <as8266749@gmail.com>
2025-01-14 18:36:46 +0530

    completed the task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,42 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let array = []
+    books.forEach(info => {
+        array.push(info.title)
+    });
+    return array
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+
+// const books = [
+//     {
+//         title: 'Book',
+//         author: 'Name'
+//     },
+//     {
+//         title: 'Book2',
+//         author: 'Name2'
+//     }
+// ]
+
+// let array = []
+// books.forEach(book => {
+
+//     array.push(book.title)
+// });
+
+// console.log(array)
+
+
+// let getTheTitles = function (book) {
+//     let array = []
+//     books.forEach(info => {
+//         array.push(info.title)
+//     })
+//     return array
+// }
+
+// console.log(getTheTitles(books))
