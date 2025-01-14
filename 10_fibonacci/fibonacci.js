d44e3392214428c0870193a2dd0901cd2b0a3fbc
Adarsh-singh-git <as8266749@gmail.com>
2025-01-14 14:17:13 +0530

    completed the task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+n = Number(n)
 
+if (n < 0) {
+    return "OOPS"
+}if (n === 0) {
+    return 0
+}if(n === 1 || n === 1){
+    return 1
+}
+
+
+let a = 1
+let b = 1 
+for (let i = 3; i <= n; i++) {
+    let next = a + b
+    a = b 
+    b = next
+    
+}
+return b
+  
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+
+
