0caa2d547df7603d58272cd2af1782775a9e3e2a
Adarsh-singh-git <as8266749@gmail.com>
2025-01-15 18:36:32 +0530

    completed the task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -10,33 +10,3 @@ const getTheTitles = function (books) {
 module.exports = getTheTitles;
 
 
-
-// const books = [
-//     {
-//         title: 'Book',
-//         author: 'Name'
-//     },
-//     {
-//         title: 'Book2',
-//         author: 'Name2'
-//     }
-// ]
-
-// let array = []
-// books.forEach(book => {
-
-//     array.push(book.title)
-// });
-
-// console.log(array)
-
-
-// let getTheTitles = function (book) {
-//     let array = []
-//     books.forEach(info => {
-//         array.push(info.title)
-//     })
-//     return array
-// }
-
-// console.log(getTheTitles(books))
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (person) {
 
+    const currentYear = new Date().getFullYear()
+    const ages = person.map(individual => {
+        const deathyear = individual.yearOfDeath || currentYear
+        return deathyear - individual.yearOfBirth
+    })
+    let maxAge = ages.reduce((largest, age) => age > largest ? age : largest, ages[0])
+
+    let oldestPersonAge = ages.indexOf(maxAge)
+
+    return person[oldestPersonAge]
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
